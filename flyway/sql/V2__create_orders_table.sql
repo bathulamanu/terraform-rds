0dbd2129e50a28d618cddcdc2b69c3f051bc1479
Ubuntu <ubuntu@ip-172-31-14-82.ap-south-1.compute.internal>
2026-06-17 10:50:21 +0000

    Add flyway and terraform folders

---
+++ flyway/sql/V2__create_orders_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE orders (
+
+    id SERIAL PRIMARY KEY,
+
+    user_id INT,
+
+    amount DECIMAL(10,2)
+
+);
---
+++ flyway/sql/V3__create_products_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE products (
+
+    id SERIAL PRIMARY KEY,
+
+    product_name VARCHAR(100),
+
+    price DECIMAL(10,2)
+
+);
